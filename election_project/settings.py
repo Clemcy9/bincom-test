309a7052ccb81ec845f8c8ecf819c6e4bf9baad7
Clement Idemudo <clemcy9@gmail.com>
2023-03-24 19:26:36 +0100

    turn debug mode off in settings.py

--- election_project/settings.py
+++ election_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%4gf&-x2^=r1(uv8r!a^sje@m0ip$m8q@_oy5xtuafk7p)kw(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'election-poll-0t3o.onrender.com',

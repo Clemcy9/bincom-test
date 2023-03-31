a55b1167003d57adbd9dad87b4a6e01898ddc9b6
Clement Idemudo <clemcy9@gmail.com>
2023-03-31 04:20:27 +0100

    added url for color choose assessment

--- dbModels/urls.py
+++ dbModels/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home,pu,lga,new_pu
+from .views import home,pu,lga,new_pu,tshirt
 
 app_name = 'dbModels'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('pu-res/',pu,name='pu_result'),
     path('lga-res/',lga,name='lga_result'),
     path('new-pu',new_pu,name='new_pu'),
+    path('tshirt',tshirt, name='tshirt')
 ]
--- dbModels/views.py
+++ dbModels/views.py
@@ -33,11 +33,29 @@ def lga(request):
 
         # making queries to get all announced result of a particular lga
         res = Lga.objects.get(lga_id=lga_req)
+        # announced result
         res1= res.announced_lga_results_set.all()
-        result_dict = {p.party_abbreviation:p.party_score for p in res1}
+        announced_result_dict = {p.party_abbreviation:p.party_score for p in res1}
         # print(result_dict)
+        
+        # calculated result
+        calc_pu = res.polling_unit_set.all()[0].announced_pu_results_set.all()
+        print(f'calc_pu:{calc_pu}')
+        # one = calc_pu[0]
+        # print(f'one:{one}')
+        # one_res = one.announced_pu_results_set.all()
+        # print(f'one_res{one_res}')
+        # calc_annouced_pu_result = [x.announced_pu_results_set.all() for x in calc_pu ]
+        # print(f'calc_announced_pu_result {calc_annouced_pu_result}')
+        # calc_pu_res = calc_pu_res.party_score
+        # party_calc_result =[]
+        # print(f'many result: {calc_pu}')
+        # print(f'one result: {type(res)}')
+        # print(f'calc polling unit: {calc_announced_polling_result}')
+        # print(f'calc polling list: {calc_announced_result_list}')
 
-        return render(request, 'lga.html', {'result_dict':result_dict,'lgas':Lga.objects.order_by('lga_name')})
+
+        return render(request, 'lga.html', {'result_dict':announced_result_dict,'lgas':Lga.objects.order_by('lga_name')})
         
     return render(request, 'lga.html',context)
 
@@ -51,3 +69,6 @@ def new_pu(request):
     else:
         f = PU_result_add_form()
     return render(request, 'new_pu_result.html',{'form':f})
+
+def tshirt(request):
+    return render(request,'shirt.html')
